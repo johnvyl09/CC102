872af9ffd5517e15be7fdd725188ad36503bab7e
john vyl alvarez <johnvylaguila07@gmail.com>
2024-10-03 10:38:58 +0800

    Add files via upload

---
+++ alvarez02BasicOutput.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std;
+
+int main ()
+{
+	cout << "Our Lady of Lourdes College Foundation" << endl;
+	cout << "Bachelor of Science in Information Technology" << endl;
+	cout << "1st year" << endl;
+	cout << "John Vyl Alvarez" << endl;
+	cout << "A.bonifacio st. Brgy.Silangan San Vicente Camarines Norte" << endl;
+	return 0;
+}
